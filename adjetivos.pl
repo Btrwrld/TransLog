9f7703594e90b91d6da606cb470244a5ab988e50
mau18alvarez <mau18alvarez@gmail.com>
2018-05-26 16:32:38 -0600

    Verbos y adjetivos
    Adjetivos, falta

---
+++ adjetivos.pl
@@ -0,0 +1,162 @@
+%Adjetivos con A
+adjetivo(aburrido,bored).
+adjetivo(ácido,acid).
+adjetivo(alto,tall).
+adjetivo(amargo,bitter).
+adjetivo(apuesto,handsome).
+adjetivo(ancho,wide).
+adjetivo(azul,blue).
+adjetivo(alegre,cheerful).
+adjetivo(atrevido,daring).
+
+%Adjetivos con B
+adjetivo(bajo,low).
+adjetivo(blanco,white).
+adjetivo(blando,soft).
+adjetivo(bonito,pretty).
+adjetivo(bueno,good).
+adjetivo(buen,good).
+adjetivo(barato,cheap).
+
+%Adjetivos con C
+adjetivo(caliente,hot).
+adjetivo(caliente,warm).
+adjetivo(capaz,capable).
+adjetivo(central,central).
+adjetivo(común,common).
+adjetivo(conocido,known).
+adjetivo(contento,happy).
+adjetivo(corto,short).
+adjetivo(cerca,near).
+adjetivo(caro,expensive).
+adjetivo(cansado,tired).
+
+%Adjetivos con D
+adjetivo(débil,weak).
+adjetivo(delgado,thin).
+adjetivo(derecho,right).
+adjetivo(diferente,different).
+adjetivo(difícil,difficult).
+adjetivo(divertido,funny).
+adjetivo(dulce,sweet).
+adjetivo(duro,hard).
+adjetivo(despejado,clear).
+adjetivo(dulce,sweet).
+
+%Adjetivos con E
+adjetivo(enfermo,ill).
+adjetivo(estrecho,tight).
+adjetivo(exterior,outside).
+
+%Adjetivos con F
+adjetivo(fácil,easy).
+adjetivo(falso,false).
+adjetivo(famoso,famous).
+adjetivo(feo,ugly).
+adjetivo(final,final).
+adjetivo(fresco,fresh).
+adjetivo(frío,cold).
+adjetivo(fuerte,strong).
+adjetivo(flaco,skinny).
+adjetivo(fiel,loyal).
+adjetivo(feliz,happy).
+
+%Adjetivos con G
+adjetivo(gordo,fat).
+adjetivo(grande,big).
+adjetivo(guapo,handsome).
+adjetivo(grueso,thick).
+adjetivo(gratis,free).
+adjetivo(gigante,giant).
+
+
+%Adjetivos con H
+adjetivo(húmedo,damp).
+
+%Adjetivos con I
+adjetivo(igual,same).
+adjetivo(imposible,impossible).
+adjetivo(interesante,interesting).
+adjetivo(inútil,useless).
+adjetivo(izquierdo,left).
+adjetivo(inteligente,smart).
+
+%Adjetivos con J
+adjetivo(joven,young).
+
+%Adjetivos con L
+adjetivo(largo,long).
+adjetivo(lento,slow).
+adjetivo(lejos,far).
+adjetivo(liviano,light).
+adjetivo(limpio,clean).
+adjetivo(lleno,full).
+
+
+%Adjetivos con M
+adjetivo(malo,bad).
+adjetivo(mejor,best).
+adjetivo(muerto,dead).
+adjetivo(mojado,wet).
+
+%Adjetivos con N.
+adjetivo(nacional,national).
+adjetivo(natural,natural).
+adjetivo(negro,black).
+adjetivo(nuevo,new).
+adjetivo(nublado,cloudy).
+
+%Adjetivos con O
+adjetivo(oscuro,dark).
+
+%Adjetivos con P
+adjetivo(peor,worse).
+adjetivo(pequeño,little).
+adjetivo(perfecto,perfect).
+adjetivo(pobre,poor).
+adjetivo(popular,popular).
+adjetivo(posible,possible).
+adjetivo(primero,first).
+adjetivo(principal,principal).
+adjetivo(pesado,heavy).
+adjetivo(profundo,deep).
+adjetivo(peligroso,dangerous).
+
+%Adjetivos con R
+adjetivo(rápido,fast).
+adjetivo(rápido,quick).
+adjetivo(raro,rare).
+adjetivo(real,real).
+adjetivo(recto,straight).
+adjetivo(rico,rich).
+adjetivo(rojo,red).
+
+%Adjetivos con S
+adjetivo(salado,salty).
+adjetivo(sano,healthy).
+adjetivo(seco,dry).
+adjetivo(segundo,second).
+adjetivo(simple,simple).
+adjetivo(social,social).
+adjetivo(solo,alone).
+adjetivo(soleado,sunny).
+adjetivo(sucio,dirty).
+
+%Adjetivos con T
+adjetivo(tímido,shy).
+adjetivo(tonto,silly).
+adjetivo(triste,sad).
+adjetivo(temprano,early).
+adjetivo(tarde,late).
+
+%Adjetivos con H
+adjetivo(hambriento,hungry).
+
+%Adjetivos con U
+adjetivo(útil,useful).
+
+%Adjetivos con V
+adjetivo(verdadero,true).
+adjetivo(verde,green).
+adjetivo(viejo,old).
+adjetivo(vivo,alive).
